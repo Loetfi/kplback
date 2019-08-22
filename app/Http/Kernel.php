c21ffada43f0bfe31ddc66d2548b8d67df201aeb
Tupay <lutfi@mhs.ubharajaya.ac.id>
2019-08-22 19:33:40 +0800

    enhance setting an session

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,7 +60,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'checkmethodpost' => \App\Http\Middleware\CheckMethodPost::class
+        'checkmethodpost' => \App\Http\Middleware\CheckMethodPost::class,
+        'checksessionlogin' => \App\Http\Middleware\SessionCheck::class,
     ];
 
     /**
